05797b9195c4c5aa9cc58083070e041454319923
Edward Rubalcaba <erubalcaba85@outlook.com>
2022-01-19 12:36:28 -0800

    hidden secret key

--- mystore/mystore/settings.py
+++ mystore/mystore/settings.py
@@ -1,6 +1,6 @@
 from oscar.defaults import *
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2e1s8dl48q(gfnyie57gy9nz-b55z&p-efub5usr=(h@=gdrha'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 
     'django.contrib.sites',
     'django.contrib.flatpages',
